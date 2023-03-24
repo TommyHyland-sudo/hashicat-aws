3c8070892b3c67901d1a81deaf0e49eb49185886
Tommy Hyland <thyland1999@gmail.com>
2023-03-24 03:57:07 +0000

    Added s3 bucket module

--- s3-bucket.tf
+++ s3-bucket.tf
@@ -2,7 +2,7 @@ module "s3_bucket" {
   source  = "app.terraform.io/training-organisation/s3-bucket/aws"
   version = "2.8.0"
 
-  bucket_prefix = "my-s3-bucket"
+  bucket = "my-s3-bucket"
   acl    = "private"
 
   versioning = {
